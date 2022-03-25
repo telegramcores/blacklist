bd6ba6a44aa60ac4ea07b3179c47674362f91c9e
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-03-25 10:39:28 +0300

    Добавил проверку интернета

--- update_bl_thunderbird.sh
+++ update_bl_thunderbird.sh
@@ -1,4 +1,6 @@
-wget https://raw.githubusercontent.com/telegramcores/blacklist/main/bl.txt -O info.txt
-find ~/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+5s/condition.*/$(<info.txt)/" {} +
-# если будут добавлены дополнительные строки в поле "Производить следующие действия:", например пересылка на другую почту, проверяем еще 7 строку
-find ~/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+7s/condition.*/$(<info.txt)/" {} +
+if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+  wget https://raw.githubusercontent.com/telegramcores/blacklist/main/bl.txt -O info.txt
+  find ~/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+5s/condition.*/$(<info.txt)/" {} +
+  # если будут добавлены дополнительные строки в поле "Производить следующие действия:", например пересылка на другую почту, проверяем еще 7 строку
+  find ~/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+7s/condition.*/$(<info.txt)/" {} +
+fi  
