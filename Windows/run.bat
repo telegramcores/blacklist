9bdca0ea579f6b11a88e613ae038c37e83c2ac7d
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-03-27 12:23:16 +0300

    Add files via upload

---
+++ Windows/run.bat
@@ -0,0 +1,23 @@
+ping ya.ru |>nul find /i "TTL=" && (
+
+start /wait wget https://raw.githubusercontent.com/telegramcores/blacklist/main/bl.txt -O info.txt
+
+chcp utf-8
+:: дополнение и исправление blacklist для 5 строки ниже
+for /F "delims=" %%i in (info.txt) do set bl="%%i"
+echo %bl%
+set bl=%bl:"=""%
+set bl=%bl:~2,-2%
+for /r "C:\test\" %%i in (msgFilterRules.dat) do (
+start sed -i "/Черный список/,+5s/condition.*/%bl%/" %%i
+)
+
+:: дополнение и исправление blacklist для 7 строки ниже
+for /F "delims=" %%i in (info.txt) do set bl="%%i"
+echo %bl%
+set bl=%bl:"=""%
+set bl=%bl:~2,-2%
+for /r "C:\test\" %%i in (msgFilterRules.dat) do (
+start sed -i "/Черный список/,+7s/condition.*/%bl%/" %%i
+)
+)
