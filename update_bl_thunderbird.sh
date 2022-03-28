283b7e2253db96299add92f15055f9088826f3a4
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-03-28 09:15:04 +0300

    Update update_bl_thunderbird.sh

--- update_bl_thunderbird.sh
+++ update_bl_thunderbird.sh
@@ -1,5 +1,5 @@
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-  wget https://raw.githubusercontent.com/telegramcores/blacklist/main/bl.txt -O info.txt
+  wget https://raw.githubusercontent.com/telegramcores/blacklist/main/info.txt -O info.txt
   find /home/*/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+5s/condition.*/$(<info.txt)/" {} +
   # если будут добавлены дополнительные строки в поле "Производить следующие действия:", например пересылка на другую почту, проверяем еще 7 строку
   find /home/*/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+7s/condition.*/$(<info.txt)/" {} +
