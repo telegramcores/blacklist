87f08bfe3acd1b1a7800e914849b4fb14a129d84
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-03-28 09:05:02 +0300

    Дополняется "черный список" для всех пользователей
    Если пользователь использует Thunderbird

--- update_bl_thunderbird.sh
+++ update_bl_thunderbird.sh
@@ -1,6 +1,6 @@
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
   wget https://raw.githubusercontent.com/telegramcores/blacklist/main/bl.txt -O info.txt
-  find ~/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+5s/condition.*/$(<info.txt)/" {} +
+  find /home/*/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+5s/condition.*/$(<info.txt)/" {} +
   # если будут добавлены дополнительные строки в поле "Производить следующие действия:", например пересылка на другую почту, проверяем еще 7 строку
-  find ~/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+7s/condition.*/$(<info.txt)/" {} +
+  find /home/*/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+7s/condition.*/$(<info.txt)/" {} +
 fi  
