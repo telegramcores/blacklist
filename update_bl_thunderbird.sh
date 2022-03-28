56cdce6bceb779eed2247955a79641658eced444
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-03-28 14:03:47 +0300

    Проверка на то, что файл не пустой.

--- update_bl_thunderbird.sh
+++ update_bl_thunderbird.sh
@@ -1,6 +1,8 @@
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
   wget -N https://raw.githubusercontent.com/telegramcores/blacklist/main/info.txt -O info.txt
-  find /home/*/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+5s/condition.*/$(<info.txt)/" {} +
-  # если будут добавлены дополнительные строки в поле "Производить следующие действия:", например пересылка на другую почту, проверяем еще 7 строку
-  find /home/*/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+7s/condition.*/$(<info.txt)/" {} +
-fi  
+    if [[ -s info.txt ]]; then
+        find /home/*/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+5s/condition.*/$(<info.txt)/" {} +
+        # если будут добавлены дополнительные строки в поле "Производить следующие действия:", например пересылка на другую почту, проверяем еще 7 строку
+        find /home/*/.thunderbird -name "msgFilterRules.dat" -exec sed -i "/Черный список/,+7s/condition.*/$(<info.txt)/" {} +
+    fi  
+fi
