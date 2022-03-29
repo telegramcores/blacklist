27077e5f156e821e7287dc19e4c25d705c97c1ea
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-03-29 20:25:47 +0300

    Update run.bat

--- Windows/run.bat
+++ Windows/run.bat
@@ -3,8 +3,9 @@
 echo.sh.Run """%~nx0"" 1", 0)>launch.vbs
 if "%~1"=="" (start "" "launch.vbs"&exit /b)
 
+Ping -n 2 1.1.1.1 2>nul|Find "TTL=">nul
+if %ERRORLEVEL% EQU 1 goto startTB
 start /wait wget https://raw.githubusercontent.com/telegramcores/blacklist/main/info.txt -O info.txt
-
 chcp 1251
 :: дополнение и исправление blacklist для 5 строки ниже
 for /F "delims=" %%i in (info.txt) do set bl="%%i"
@@ -20,4 +21,6 @@ set bl=%bl:~2,-2%
 for /r "Data\profile\ImapMail\" %%i in (msgFilterRules.dat) do (
 start sed -i "/Черный список/,+7s/condition.*/%bl%/" %%i
 )
+
+:startTB
 ThunderbirdPortable.exe
