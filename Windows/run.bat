4d8485664dff01800a5876ab3435e45c8063f293
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-03-29 11:46:55 +0300

    Update run.bat

--- Windows/run.bat
+++ Windows/run.bat
@@ -1,21 +1,23 @@
-ping ya.ru |>nul find /i "TTL=" && (
+@echo off
+(echo.set sh=CreateObject^("Wscript.Shell"^)
+echo.sh.Run """%~nx0"" 1", 0)>launch.vbs
+if "%~1"=="" (start "" "launch.vbs"&exit /b)
 
 start /wait wget https://raw.githubusercontent.com/telegramcores/blacklist/main/info.txt -O info.txt
 
-chcp utf-8
+chcp 1251
 :: дополнение и исправление blacklist для 5 строки ниже
 for /F "delims=" %%i in (info.txt) do set bl="%%i"
 set bl=%bl:"=""%
 set bl=%bl:~2,-2%
-for /r "C:\test\" %%i in (msgFilterRules.dat) do (
+for /r "Data\profile\ImapMail\" %%i in (msgFilterRules.dat) do (
 start sed -i "/Черный список/,+5s/condition.*/%bl%/" %%i
 )
-
 :: дополнение и исправление blacklist для 7 строки ниже
 for /F "delims=" %%i in (info.txt) do set bl="%%i"
 set bl=%bl:"=""%
 set bl=%bl:~2,-2%
-for /r "C:\test\" %%i in (msgFilterRules.dat) do (
+for /r "Data\profile\ImapMail\" %%i in (msgFilterRules.dat) do (
 start sed -i "/Черный список/,+7s/condition.*/%bl%/" %%i
 )
-)
+ThunderbirdPortable.exe
